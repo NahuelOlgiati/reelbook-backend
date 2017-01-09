ff296d83bff358772d0f5a9ff8db6eabd66a5ca5
nahuel <nahuelolgiati@hotmail.com>
2017-01-09 00:57:17 -0300

    stream test

--- src/main/java/com/reelbook/rest/endpoint/AudioVisualEndPoint.java
+++ src/main/java/com/reelbook/rest/endpoint/AudioVisualEndPoint.java
@@ -1,5 +1,10 @@
 package com.reelbook.rest.endpoint;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Map;
 import java.util.Map.Entry;
 import javax.ejb.EJB;
@@ -14,8 +19,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import com.reelbook.core.endpoint.BaseManagerEnpoint;
 import com.reelbook.core.exception.ManagerException;
@@ -29,20 +37,19 @@ import com.reelbook.service.manager.local.AudioVisualManagerLocal;
 
 @Stateless
 @Path("/audiovisual")
-public class AudioVisualEndPoint extends BaseManagerEnpoint<AudioVisual>
-{
+public class AudioVisualEndPoint extends BaseManagerEnpoint<AudioVisual> {
 	@EJB
 	private AudioVisualManagerLocal audioVisualML;
 
 	// @GET
 	// @RequiredRole({RoleEnum.ADMIN})
 	// @Produces(MediaType.APPLICATION_JSON)
-	public Response getList()
-	{
+	public Response getList() {
 		Response r = null;
 		// try
 		// {
-		// r = ResponseUtil.success(audioVisualML.getQueryHintResult("", new QueryHint(0, Integer.MAX_VALUE)).getQueryList().toArray(new
+		// r = ResponseUtil.success(audioVisualML.getQueryHintResult("", new
+		// QueryHint(0, Integer.MAX_VALUE)).getQueryList().toArray(new
 		// AudioVisual[0]));
 		// }
 		// catch (Exception e)
@@ -56,15 +63,11 @@ public class AudioVisualEndPoint extends BaseManagerEnpoint<AudioVisual>
 	@GET
 	@Path("/get:{id:[0-9][0-9]*}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response get(@PathParam("id") Long id)
-	{
+	public Response get(@PathParam("id") Long id) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(audioVisualML.get(id));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -75,13 +78,15 @@ public class AudioVisualEndPoint extends BaseManagerEnpoint<AudioVisual>
 	// @GET
 	// @Path("/pagedlist:{description}")
 	// @Produces(MediaType.APPLICATION_JSON)
-	public Response pagedlist(@PathParam("description") String description, @DefaultValue("0") @QueryParam("firstResult") Integer firstResult,
-			@DefaultValue("10") @QueryParam("maxResults") Integer maxResults)
-	{
+	public Response pagedlist(@PathParam("description") String description,
+			@DefaultValue("0") @QueryParam("firstResult") Integer firstResult,
+			@DefaultValue("10") @QueryParam("maxResults") Integer maxResults) {
 		Response r = null;
 		// try
 		// {
-		// r = ResponseUtil.success(audioVisualML.getQueryHintResult(description, new QueryHint(firstResult, maxResults)));
+		// r =
+		// ResponseUtil.success(audioVisualML.getQueryHintResult(description,
+		// new QueryHint(firstResult, maxResults)));
 		// }
 		// catch (Exception e)
 		// {
@@ -94,15 +99,11 @@ public class AudioVisualEndPoint extends BaseManagerEnpoint<AudioVisual>
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response create(AudioVisual audioVisual)
-	{
+	public Response create(AudioVisual audioVisual) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(audioVisualML.save(audioVisual));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -112,15 +113,11 @@ public class AudioVisualEndPoint extends BaseManagerEnpoint<AudioVisual>
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response update(AudioVisual audioVisual)
-	{
+	public Response update(AudioVisual audioVisual) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(audioVisualML.save(audioVisual));
-		}
-		catch (ManagerException e)
-		{
+		} catch (ManagerException e) {
 			System.out.println("exception in update " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -130,15 +127,11 @@ public class AudioVisualEndPoint extends BaseManagerEnpoint<AudioVisual>
 	@DELETE
 	@Path("/{id:[0-9][0-9]*}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response delete(@PathParam("id") Long id)
-	{
+	public Response delete(@PathParam("id") Long id) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(audioVisualML.delete(id));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -148,59 +141,27 @@ public class AudioVisualEndPoint extends BaseManagerEnpoint<AudioVisual>
 	@POST
 	@Path("/submit")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response uploadFile(MultipartFormDataInput input)
-	{
+	public Response uploadFile(MultipartFormDataInput input) {
 		String uploadName = "uploadedFile";
 		Integer bufferSize = 8192;
 		Response r = null;
-		try
-		{
+		try {
 			String token = input.getFormDataPart("token", String.class, null);
 			UserPrincipal authenticatedUser = UserPrincipalMap.get(token);
 			Long userID = authenticatedUser.getUser().getID();
 
 			Map<String, byte[]> bytesMap = FileUtil.getByteArrayMap(input, uploadName, bufferSize);
-			for (Entry<String, byte[]> entry : bytesMap.entrySet())
-			{
+			for (Entry<String, byte[]> entry : bytesMap.entrySet()) {
 				String fileName = entry.getKey();
 				byte[] content = entry.getValue();
 				audioVisualML.addVideo(userID, new Video(fileName, content));
 			}
 			r = ResponseUtil.success();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
 		}
 		return r;
 
 	}
-
-	// @GET
-	// @Produces(MediaType.TEXT_PLAIN)
-	// @Path("/{nodeId}/{depth}")
-	// public Response hello(@PathParam("nodeId") long nodeId, @PathParam("depth") int depth) {
-	// Node node = database.getNodeById(nodeId);
-	//
-	// final Traverser paths = Traversal.description()
-	// .depthFirst()
-	// .relationships(DynamicRelationshipType.withName("whatever"))
-	// .evaluator( Evaluators.toDepth(depth) )
-	// .traverse(node);
-	//
-	// StreamingOutput stream = new StreamingOutput() {
-	// @Override
-	// public void write(OutputStream os) throws IOException, WebApplicationException {
-	// Writer writer = new BufferedWriter(new OutputStreamWriter(os));
-	//
-	// for (org.neo4j.graphdb.Path path : paths) {
-	// writer.write(path.toString() + "\n");
-	// }
-	// writer.flush();
-	// }
-	// };
-	//
-	// return Response.ok(stream).build();
-	// }
 }
--- src/main/java/com/reelbook/rest/endpoint/UserEndPoint.java
+++ src/main/java/com/reelbook/rest/endpoint/UserEndPoint.java
@@ -1,5 +1,12 @@
 package com.reelbook.rest.endpoint;
 
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
@@ -12,40 +19,40 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import com.reelbook.core.endpoint.BaseManagerEnpoint;
 import com.reelbook.core.exception.ManagerException;
 import com.reelbook.core.model.support.QueryHint;
+import com.reelbook.model.AudioVisual;
 import com.reelbook.model.User;
 import com.reelbook.rest.annotation.RequiredRole;
 import com.reelbook.rest.app.RoleEnum;
 import com.reelbook.rest.util.ResponseUtil;
+import com.reelbook.service.manager.local.AudioVisualManagerLocal;
 import com.reelbook.service.manager.local.FileManagerLocal;
 import com.reelbook.service.manager.local.UserManagerLocal;
 
 @Stateless
 @Path("/user")
-public class UserEndPoint extends BaseManagerEnpoint<User>
-{
+public class UserEndPoint extends BaseManagerEnpoint<User> {
 	@EJB
 	private UserManagerLocal userML;
 	@EJB
 	private FileManagerLocal fileML;
 
 	@GET
-	@RequiredRole({RoleEnum.ADMIN})
+	@RequiredRole({ RoleEnum.ADMIN })
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getList()
-	{
+	public Response getList() {
 		Response r = null;
-		try
-		{
-			r = ResponseUtil.success(userML.getQueryHintResult("", new QueryHint(0, Integer.MAX_VALUE)).getQueryList().toArray(new User[0]));
-		}
-		catch (Exception e)
-		{
+		try {
+			r = ResponseUtil.success(userML.getQueryHintResult("", new QueryHint(0, Integer.MAX_VALUE)).getQueryList()
+					.toArray(new User[0]));
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -55,15 +62,11 @@ public class UserEndPoint extends BaseManagerEnpoint<User>
 	@GET
 	@Path("/get:{id:[0-9][0-9]*}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response get(@PathParam("id") Long id)
-	{
+	public Response get(@PathParam("id") Long id) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(userML.get(id));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -74,16 +77,13 @@ public class UserEndPoint extends BaseManagerEnpoint<User>
 	@GET
 	@Path("/pagedlist:{description}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response pagedlist(@PathParam("description") String description, @DefaultValue("0") @QueryParam("firstResult") Integer firstResult,
-			@DefaultValue("10") @QueryParam("maxResults") Integer maxResults)
-	{
+	public Response pagedlist(@PathParam("description") String description,
+			@DefaultValue("0") @QueryParam("firstResult") Integer firstResult,
+			@DefaultValue("10") @QueryParam("maxResults") Integer maxResults) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(userML.getQueryHintResult(description, new QueryHint(firstResult, maxResults)));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -93,15 +93,11 @@ public class UserEndPoint extends BaseManagerEnpoint<User>
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response create(User user)
-	{
+	public Response create(User user) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(userML.save(user));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -111,15 +107,11 @@ public class UserEndPoint extends BaseManagerEnpoint<User>
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response update(User user)
-	{
+	public Response update(User user) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(userML.save(user));
-		}
-		catch (ManagerException e)
-		{
+		} catch (ManagerException e) {
 			System.out.println("exception in update " + e);
 			r = ResponseUtil.fatalException();
 		}
@@ -129,17 +121,46 @@ public class UserEndPoint extends BaseManagerEnpoint<User>
 	@DELETE
 	@Path("/{id:[0-9][0-9]*}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response delete(@PathParam("id") Long id)
-	{
+	public Response delete(@PathParam("id") Long id) {
 		Response r = null;
-		try
-		{
+		try {
 			r = ResponseUtil.success(userML.delete(id));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("exception in create " + e);
 			r = ResponseUtil.fatalException();
+		}
+		return r;
+	}
+
+	@EJB
+	private AudioVisualManagerLocal audioVisualML;
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/stream")
+	public StreamingOutput stream() {
+		StreamingOutput r = null;
+		try {
+			AudioVisual audioVisual = audioVisualML.get(1l);
+			ByteArrayInputStream bis = new ByteArrayInputStream(audioVisual.getVideo().getContent());
+
+			StreamingOutput stream = new StreamingOutput() {
+				@Override
+				public void write(OutputStream output) throws IOException, WebApplicationException {
+					Writer writer = new BufferedWriter(new OutputStreamWriter(output));
+
+					int b;
+					while ((b = bis.read()) != -1) {
+						writer.write(b);
+
+					}
+					writer.flush();
+				}
+			};
+			r = stream;
+		} catch (Exception e) {
+			System.out.println("exception in create " + e);
+			r = null;
 		}
 		return r;
 	}
